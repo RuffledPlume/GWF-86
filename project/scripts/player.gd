fc8de3b6c62cd31869ea26a94cb15c21d3c0e49c
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2025-10-13 09:02:13 +0100

    Very Basic Movement

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -1,1 +1,17 @@
 class_name Player extends Character
+
+func _physics_process(delta: float) -> void:
+	var movement_input : Vector3
+	if Input.is_key_pressed(KEY_D):
+		movement_input.x = 1.0
+	elif Input.is_key_pressed(KEY_A):
+		movement_input.x = -1.0
+	
+	if Input.is_key_pressed(KEY_W):
+		movement_input.z = 1.0
+	elif Input.is_key_pressed(KEY_S):
+		movement_input.z = -1.0
+		
+	velocity = movement_input * 100.0 * delta
+	
+	move_and_slide()
